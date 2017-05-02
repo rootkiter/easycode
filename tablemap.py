6635c326b4355dde1e5b32ae811f8b063c3a242e
rootkiter <rootkiter@rootkiter.com>
2017-05-02 17:32:25 +0800

    updata tableTitle

--- tablemap.py
+++ tablemap.py
@@ -8,20 +8,26 @@
 
 class tablemap:
     def __init__(self):
-        self.titlelist={}
+        self.tableTitle=""
+        self.title={}
+        self.titlelist=[]
         self.item = {}
         self.itemnamelen=0
         self.itemlist = []
 
+    def setTitle(self,title):
+        self.tableTitle = title
+
     def additem(self,title,itemname,itemvalue):
-        if(title not in self.titlelist):
-            self.titlelist[title]=len(title)
+        if(title not in self.title):
+            self.title[title]=len(title)
+            self.titlelist.append(title)
         if(itemname not in self.item):
             self.item[itemname] = {}
             self.itemlist.append(itemname)
         self.item[itemname][title]=str(itemvalue)
-        if(self.titlelist[title] < len(str(itemvalue))):
-            self.titlelist[title] = len(str(itemvalue))
+        if(self.title[title] < len(str(itemvalue))):
+            self.title[title] = len(str(itemvalue))
         if(len(itemname) > self.itemnamelen):
             self.itemnamelen = len(itemname)
         return True
@@ -34,13 +40,13 @@ class tablemap:
             for citem in List:
                 if(citem.startswith('-')):
                     item = citem[1::]
-                    formatString = " %%-%ds |" % self.titlelist[item]
-                    itembuf = (((self.titlelist[item] - len(item))/2)*' ')+item
+                    formatString = " %%-%ds |" % self.title[item]
+                    itembuf = (((self.title[item] - len(item))/2)*' ')+item
                     result += formatString % (itembuf)
                 else:
                     item = citem
-                    formatString = " %%%ds |" % self.titlelist[item]
-                    itembuf = item+(((self.titlelist[item] - len(item))/2)*' ')
+                    formatString = " %%%ds |" % self.title[item]
+                    itembuf = item+(((self.title[item] - len(item))/2)*' ')
                     result += formatString % (itembuf)
         else:
             formatString = "%%-%ds |" % self.itemnamelen
@@ -48,10 +54,10 @@ class tablemap:
             for citem in List:
                 if(citem.startswith('-')):
                     item = citem[1::]
-                    formatString = " %%-%ds |" % self.titlelist[item]
+                    formatString = " %%-%ds |" % self.title[item]
                 else:
                     item = citem
-                    formatString = " %%%ds |" % self.titlelist[item]
+                    formatString = " %%%ds |" % self.title[item]
                 try:
                     result += formatString % str(itemList[item])
                 except Exception, e:
@@ -64,20 +70,31 @@ class tablemap:
         lineK        = "+-"
         List = []
         if(titleList == None):
-            for item in self.titlelist :
-                List .append(item)
+            List = self.titlelist
         else:
             List = titleList
+
+        for item in List:
+            if(item.replace('-','') not in self.titlelist):
+                return ""
         formatString += "%%%ds |" % self.itemnamelen
         lineK += self.itemnamelen*'-' + "-+"
         for citem in List:
             if(citem.startswith('-')):
                 item=citem[1::]
-                formatString += " %%-%ds |" % self.titlelist[item]
+                formatString += " %%-%ds |" % self.title[item]
             else:
                 item=citem
-                formatString += " %%%ds |" % self.titlelist[item]
-            lineK += "-"+ self.titlelist[item]*'-' + "-+"
+                formatString += " %%%ds |" % self.title[item]
+            lineK += "-"+ self.title[item]*'-' + "-+"
+
+        if(len(self.tableTitle)>0):
+            maxlen = len(lineK)
+            noplen = (maxlen-len(self.tableTitle)-4)/2
+            print noplen
+            TBTitle = " "*noplen+"*"+self.tableTitle +"*"
+            result += "+"+'-'*(maxlen-2)+"+" +"\n"
+            result += "|"+TBTitle +" "*(maxlen-2-len(TBTitle))+"|\n"
 
         result += lineK +"\n"
         result += self.getItemString(List,"") + "\n"
@@ -116,4 +133,5 @@ if __name__=='__main__':
     | item2 |       Hello 2*2 | Hello 2*1 BBBBB |
     +-------+-----------------+-----------------+
     '''
+    tbmap.setTitle("Hello Table")
     print tbmap.printMap(['title2','-title1'])
